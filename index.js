2002e85c93adf2a28c75af5cacd25ceec3c7d8dc
Jethro Kuan <jethrokuan95@gmail.com>
2016-09-10 11:31:33 +0800

    Basics

---
+++ index.js
@@ -0,0 +1,21 @@
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = '280956301:AAGSeA8De_HcjKLDhgwIt8odRILZhJf8sj4';
+
+// Setup polling way
+var bot = new TelegramBot(token, {polling: true});
+
+// Matches /echo [whatever]
+bot.onText(/\/echo (.+)/, function (msg, match) {
+    var fromId = msg.from.id;
+    var resp = match[1];
+    bot.sendMessage(fromId, resp);
+});
+
+// Any kind of message
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id;
+    // photo can be: a file path, a stream or a Telegram file_id
+    var photo = 'cats.png';
+    bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
+});
